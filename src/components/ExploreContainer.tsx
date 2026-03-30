da61310729d1f71a3766323e026aedb0078b3644
cha2604 <Charitysalinas09@gmail.com>
2026-03-30 08:59:24 +0800

    intial commit

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import type { JSX } from 'react';
+import './ExploreContainer.css';
+import { IonFab, IonFabButton, IonIcon, IonFabList } from '@ionic/react';
+import {
+  chevronDownCircle,
+  chevronForwardCircle,
+  chevronUpCircle,
+  colorPalette,
+  document,
+  globe,
+} from 'ionicons/icons';
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <IonFab slot="fixed" vertical="bottom" horizontal="end">
+          <IonFabButton>
+            <IonIcon icon={chevronUpCircle}></IonIcon>
+          </IonFabButton>
+          <IonFabList side="top">
+            <IonFabButton>
+              <IonIcon icon={document}></IonIcon>
+            </IonFabButton>
+            <IonFabButton>
+              <IonIcon icon={colorPalette}></IonIcon>
+            </IonFabButton>
+            <IonFabButton>
+              <IonIcon icon={globe}></IonIcon>
+            </IonFabButton>
+          </IonFabList>
+        </IonFab>
+  );
+};
+
+export default ExploreContainer;
